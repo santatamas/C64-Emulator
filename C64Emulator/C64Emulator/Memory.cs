63b508ed85f9eee535bfd0eb109240b705132ec5
illesj <jozsef_illes@epam.com>
2015-05-19 17:47:54 +0200

    implement initial memory

--- C64Emulator/C64Emulator/Memory.cs
+++ C64Emulator/C64Emulator/Memory.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace C64Emulator
 {
-    class Memory
+    public class Memory
     {
+        public const int Size = 65536;
+
+        private readonly byte[] memory = new byte[Size];
+
+        public byte ReadZeroPage(byte zeroPageAddress)
+        {
+            return memory[zeroPageAddress];
+        }
+
+        public byte ReadAbsolute(ushort absoluteAddress)
+        {
+            return memory[absoluteAddress];
+        }
+
+        public void WriteZeroPage(byte zeroPageAddress, byte value)
+        {
+            memory[zeroPageAddress] = value;
+        }
+
+        public void WriteAbsolute(ushort absoluteAddress, byte value)
+        {
+            memory[absoluteAddress] = value;
+        }
     }
 }
