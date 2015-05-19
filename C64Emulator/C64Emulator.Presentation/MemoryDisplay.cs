703b7ed0f72438c9cd8533434731c26da948ca18
santatamas <thomas.felis@gmail.com>
2015-05-19 19:16:12 +0200

    screen!

--- C64Emulator/C64Emulator.Presentation/MemoryDisplay.cs
+++ C64Emulator/C64Emulator.Presentation/MemoryDisplay.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using Hackaton;
 
 namespace C64Emulator.Presentation
 {
     public class MemoryDisplay
     {
+        private ushort _screenStartAddress = 0x400;
         private Memory _memory;
+        private byte _width = 40;
+        private byte _height = 25;
+
         public MemoryDisplay(Memory memory)
         {
             _memory = memory;
@@ -12,12 +17,14 @@ namespace C64Emulator.Presentation
 
         public char[,] ReadCurrentState()
         {
-            var result = new char[100,100];
-            var temp = DateTime.Now.ToString().ToCharArray();
-
-            for (int index = 0; index < temp.Length; index++)
+            var result = new char[_width, _height];
+            var currentAdr = _screenStartAddress;
+            for (byte x = 0; x < _width; x++)
             {
-                result[0,index] = temp[index];
+                for (byte y = 0; y < _height; y++)
+                {
+                    result[x, y] = C64CharConverter.ConvertToAscii(_memory.ReadAbsolute(currentAdr++));
+                }
             }
             return result;
         }
