c0edddd52f48ca79a97d71569c4ec15051568481
aborszeki <aborszeki@gmail.com>
2015-05-19 19:58:31 +0200

    +

---
+++ C64Emulator/C64Emulator/Instructions/BNE.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hackaton;
+
+namespace C64Emulator
+{
+    partial class Processor
+    {
+        private void BNE(AddressingMode mode)
+        {
+            PC++;
+            if (Zero)
+
+        }
+    }
+}
